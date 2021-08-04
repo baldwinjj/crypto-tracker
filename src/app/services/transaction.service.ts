8c4c599198384742eaae5161a77f4d678c2680ca
baldwinjj <sjbaldwin94@gmail.com>
2021-08-03 22:59:05 -0400

    Add transaction service

--- src/app/services/transaction.service.ts
+++ src/app/services/transaction.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import {ITransaction} from "../interfaces/ITransaction";
+import {Observable, of} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransactionService {
+  /** In Memory store of transactions */
+  private transactions: ITransaction[] = []
 
   constructor() { }
+
+  getTransactions(): Observable<ITransaction[]> {
+    return of(this.transactions);
+  }
+
+  createTransaction(transaction: ITransaction) {
+    this.transactions.push(transaction);
+  }
+
+  deleteTransaction(index: number) {
+    this.transactions.splice(index, 1);
+  }
+
+  updateTransaction(index: number, transaction: ITransaction) {
+    this.transactions[index] = transaction;
+  }
 }
