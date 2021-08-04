6fd51c4c45522e4d5d253d93990c2c4befe6cc43
baldwinjj <sjbaldwin94@gmail.com>
2021-08-03 22:48:56 -0400

    Create rates requests in rates service

--- src/app/services/rate.service.ts
+++ src/app/services/rate.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import {ICurrency} from "../interfaces/ICurrency";
+import {IRate} from "../interfaces/IRate";
+import {forkJoin, Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {map} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class RateService {
+  RATE_URL = 'https://bitpay.com/api/rates/'
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  /**
+   * Returns a map of rates
+   * @param codes
+   */
+  getRates(codes: ICurrency['code'][]): Observable<{ [key: string]: IRate }> {
+    const urlMap = codes.reduce((acc, code) => ({
+      ...acc,
+      [code]: this.getRate(code)
+    }), {})
+    return forkJoin(urlMap)
+  }
+
+  /**
+   * Returns a rate for a single code
+   * @param code
+   */
+  getRate(code: ICurrency['code']): Observable<IRate> {
+    return this.httpClient.get<IRate>(this.RATE_URL + code + '/USD')
+  }
 }
